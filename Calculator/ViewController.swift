aa5f63abe69eecc0924b3ae5f5641759cffca875
ForeverQ <ForeverQ@qianheng-MacBook-Pro.local>
2015-11-24 20:27:48 +0800

    First Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 钱恒 on 15/11/23.
+//  Copyright © 2015年 East China University of Science and Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender
+    }
+    
+}
+
