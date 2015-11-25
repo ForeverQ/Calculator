9f5fd1c011f823a45832235024cd3a79014f0dc5
ForeverQ <ForeverQ@qianheng-MacBook-Pro.local>
2015-11-25 23:08:03 +0800

    Display Numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,8 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
     @IBAction func appendDigit(sender: UIButton) {
-        let digit = sender
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
     
 }
